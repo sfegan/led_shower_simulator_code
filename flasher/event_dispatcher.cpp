a0a5629b9f49f2a2392e77bdf13f3fa2c046b04c
Stephen Fegan <sfegan@gmail.com>
2024-02-05 11:56:02 +0100

    Add PIO program to set charges

--- flasher/event_dispatcher.cpp
+++ flasher/event_dispatcher.cpp
@@ -1,7 +1,10 @@
+#include <cstdlib>
+
 #include <pico/stdlib.h>
 #include <pico/multicore.h>
 
 #include "event_dispatcher.hpp"
+#include "set_charges.pio.h"
 
 EventDispatcher::EventDispatcher()
 {
@@ -15,6 +18,21 @@ void EventDispatcher::launch_dispatcher_thread()
 
 void EventDispatcher::run_dispatcher_loop()
 {
+    // Choose which PIO instance to use (there are two instances)
+    PIO pio = pio0;
+
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offset = pio_add_program(pio, &set_charges_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint sm = pio_claim_unused_sm(pio, true);
+    set_charges_program_init(pio, sm, offset, 0, 20);
+
     bool state = 0;
     lock();
     while(run_dispatcher_) {
@@ -22,7 +40,11 @@ void EventDispatcher::run_dispatcher_loop()
         dispatcher_running_ = true;
         gpio_put(PICO_DEFAULT_LED_PIN, state);
         state = 1 - state;
-        sleep_ms(1000);
+        uint x = rand() & 0xFFFF;
+        //x |= x<<16;
+        x <<= 16;
+        pio_sm_put_blocking(pio, sm, x);
+        sleep_ms(100);
         lock();
     }
     dispatcher_running_ = false;
